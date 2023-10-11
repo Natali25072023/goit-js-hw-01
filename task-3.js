cf9d83456fff3ea3167e74f9a0b20667d3ebfa9c
Natali25072023 <infkab2@gmail.com>
2023-10-11 19:13:24 +0300

    vers-2

--- task-3.js
+++ task-3.js
@@ -1,5 +1,5 @@
 function getElementWidth(content, padding, border) { 
-    console.log(content+padding+padding+border+border);
+    console.log(Number.parseFloat(content)+2*Number.parseFloat(padding) +2*Number.parseFloat(border+border));
 }
 getElementWidth("50px", "8px", "4px");
 getElementWidth("60px", "12px", "8.5px");
